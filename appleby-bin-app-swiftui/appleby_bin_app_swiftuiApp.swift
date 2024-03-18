dc074620da981758e2a7c7c7db981786227bd4ec
Roy Moody <roy@moodyweb.co.uk>
2024-03-18 19:30:32 +0000

    Initial Commit

---
+++ appleby-bin-app-swiftui/appleby_bin_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleby_bin_app_swiftuiApp.swift
+//  appleby-bin-app-swiftui
+//
+//  Created by roy.moody on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct appleby_bin_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
